fe001a2c82118a4b6c6e0d4124d29028e5d17e47
23-faisal <a.faisal23r@gmail.com>
2024-09-21 20:36:42 +0600

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import AuthLayout from "./components/ui/auth/Layout";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import AdminLayout from "./components/ui/admin-view/layout";
+import AdminDashboard from "./pages/admin-view/Dashboard/Dashboard";
+import AdminFeatures from "./pages/admin-view/Features/Features";
+import AdminOrders from "./pages/admin-view/Orders/Orders";
+import AdminProducts from "./pages/admin-view/Products/Products";
+import PageNotFound from "./pages/page not found/PageNotFound";
+import ShoppingLayout from "./components/shopping-view/layout";
+import ShoppingHome from "./pages/shopping-view/Home/Home";
+import ShoppingListing from "./pages/shopping-view/Product /Listing";
+import ShoppingCheckOut from "./pages/shopping-view/Checkout/CheckOut";
+import ShoppingAccount from "./pages/shopping-view/Account/Account";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col overflow-hidden bg-white">
+      <h1>Header component</h1>
+      <Routes>
+        <Route path="/auth/*" element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+        <Route path="/admin/*" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="features" element={<AdminFeatures />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="products" element={<AdminProducts />} />
+        </Route>
+
+        <Route path="/shop/*" element={<ShoppingLayout />}>
+          <Route path="home" element={<ShoppingHome />} />
+          <Route path="listing" element={<ShoppingListing />} />
+          <Route path="checkout" element={<ShoppingCheckOut />} />
+          <Route path="account" element={<ShoppingAccount />} />
+        </Route>
+
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/shopping-view/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ShoppingHeader = () => {
+  return (
+    <div>
+      <h1>Shopping view header</h1>
+    </div>
+  );
+};
+
+export default ShoppingHeader;
